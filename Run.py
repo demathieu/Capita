c8dd58f0d7b263962acc297a446eef44c2b93670
demathieu <mathieuthys@hotmail.com>
2015-06-15 21:30:49 +0200

    New
    Some more function

--- Run.py
+++ Run.py
@@ -4,6 +4,7 @@ import unittest
 import LexTest as LexTest
 import YaccTestVarDecla as YaccTestVarDecla
 import YaccTestSimpleFunction as YaccTestSimpleFunction
+import YaccTestLoops as YaccTestLoops
 
 
 testList = [
@@ -36,15 +37,19 @@ testList = [
 ['NAME','naam']
 ]
 suite = unittest.TestSuite()
-for test in testList:
-     if test != None:
-         suite.addTest(LexTest.ParametrizedTestCase.parametrize(LexTest.TestTokenDefault, param =test))
-suite.addTest(LexTest.ParametrizedTestCase.parametrize(LexTest.TestTokenName))
-suite.addTest(LexTest.ParametrizedTestCase.parametrize(LexTest.TestTokenNumber))
-suite.addTest(YaccTestVarDecla.ParametrizedTestCase.parametrize(YaccTestVarDecla.TestIntegerVarDeclarations))
-suite.addTest(YaccTestVarDecla.ParametrizedTestCase.parametrize(YaccTestVarDecla.TestCharVarDeclarations))
-suite.addTest(YaccTestVarDecla.ParametrizedTestCase.parametrize(YaccTestVarDecla.TestIntegerAndCharCombined))
-suite.addTest(YaccTestSimpleFunction.ParametrizedTestCase.parametrize(YaccTestSimpleFunction.TestSimpleFunctionNoParamsNoBody))
-suite.addTest(YaccTestSimpleFunction.ParametrizedTestCase.parametrize(YaccTestSimpleFunction.TestSimpleFunctionNoParamsWithSimpleBodyAndVarDeclaration))
-suite.addTest(YaccTestSimpleFunction.ParametrizedTestCase.parametrize(YaccTestSimpleFunction.TestSimpleFunctionNoParamsWithVarInitialization))
+# for test in testList:
+#      if test != None:
+#          suite.addTest(LexTest.ParametrizedTestCase.parametrize(LexTest.TestTokenDefault, param =test))
+# suite.addTest(LexTest.ParametrizedTestCase.parametrize(LexTest.TestTokenName))
+# suite.addTest(LexTest.ParametrizedTestCase.parametrize(LexTest.TestTokenNumber))
+# suite.addTest(YaccTestVarDecla.ParametrizedTestCase.parametrize(YaccTestVarDecla.TestIntegerVarDeclarations))
+# suite.addTest(YaccTestVarDecla.ParametrizedTestCase.parametrize(YaccTestVarDecla.TestCharVarDeclarations))
+# suite.addTest(YaccTestVarDecla.ParametrizedTestCase.parametrize(YaccTestVarDecla.TestIntegerAndCharCombined))
+# suite.addTest(YaccTestSimpleFunction.ParametrizedTestCase.parametrize(YaccTestSimpleFunction.TestSimpleFunctionNoParamsNoBody))
+# suite.addTest(YaccTestSimpleFunction.ParametrizedTestCase.parametrize(YaccTestSimpleFunction.TestSimpleFunctionNoParamsWithSimpleBodyAndVarDeclaration))
+# suite.addTest(YaccTestSimpleFunction.ParametrizedTestCase.parametrize(YaccTestSimpleFunction.TestSimpleFunctionNoParamsWithVarInitialization))
+# suite.addTest(YaccTestLoops.ParametrizedTestCase.parametrize(YaccTestLoops.TestIfClauseWithoutElse))
+# suite.addTest(YaccTestLoops.ParametrizedTestCase.parametrize(YaccTestLoops.TestIfClauseWitElseButNotUsed))
+#suite.addTest(YaccTestLoops.ParametrizedTestCase.parametrize(YaccTestLoops.TestIfClauseBody))
+suite.addTest(YaccTestLoops.ParametrizedTestCase.parametrize(YaccTestLoops.TestWhileLoop))
 unittest.TextTestRunner(verbosity=3).run(suite)
